6eac8e9a0f613cef7b87c4f996131dc3b2a34283
Pu Wang <pwang@smartline.com.au>
2022-03-13 01:18:57 +0800

    Add the test for serialize response

--- src/test/java/com/ticketbook/order/controller/OrderControllerTest.java
+++ src/test/java/com/ticketbook/order/controller/OrderControllerTest.java
@@ -2,6 +2,7 @@ package com.ticketbook.order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ticketbook.order.dto.InvoiceRequestDto;
+import com.ticketbook.order.dto.InvoiceResponseDto;
 import com.ticketbook.order.model.InvoiceRequest;
 import com.ticketbook.order.service.OrderService;
 import com.ticketbook.order.service.exception.FlightIsNotFinishedException;
@@ -15,10 +16,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.UUID;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -96,4 +101,29 @@ public class OrderControllerTest {
         .andExpect(status().isBadRequest())
         .andExpect(content().string(containsString("Flight with id 6X5CAB is not finished.")));
   }
+
+  @Test
+  public void requestInvoice_should_return_201_when_all_good() throws Exception {
+    InvoiceRequestDto request = InvoiceRequestDto.builder().email("test@gmail.com").build();
+
+    InvoiceRequest invoiceRequest = InvoiceRequest.builder()
+        .orderId("AH597C")
+        .ticketId("af12f6")
+        .email("test@gmail.com")
+        .build();
+
+    UUID invoiceId = UUID.randomUUID();
+    when(orderService.requestInvoice(invoiceRequest)).thenReturn(invoiceId);
+
+    MvcResult mvcResult = mockMvc.perform(post("/orders/AH597C/tickets/af12f6/invoice")
+        .content(objectMapper.writeValueAsString(request))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated())
+        .andReturn();
+
+    String response = mvcResult.getResponse().getContentAsString();
+    InvoiceResponseDto expected = InvoiceResponseDto.builder().invoiceRequestId(invoiceId).build();
+    assertEquals(response, objectMapper.writeValueAsString(expected));
+  }
+
 }
